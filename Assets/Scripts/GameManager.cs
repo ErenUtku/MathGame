3f58501b58a7e4b57bbda67d8a2a5e2caa1fa229
ErenUtku <61837087+ErenUtku@users.noreply.github.com>
2021-11-08 16:28:21 +0300

    Init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Problem[] problems;      
+    public int curProblem;          
+    public float timePerProblem;    
+    public float remainingTime;     
+    public Player player;
+
+    public static GameManager instance;
+    void Awake()
+    {
+        instance = this;  // set instance to this script.(singleton) Accessing purposes(GameManager.instance)
+    }
+    void Start()
+    {
+        SetProblem(0);
+    }
+    void Win()
+    {
+        Time.timeScale = 0.0f;
+        UI.instance.SetEndText(true);
+    }
+    void Lose()
+    {
+        Time.timeScale = 0.0f;
+        UI.instance.SetEndText(false);
+    }
+    void SetProblem(int problem)
+    {
+       
+        curProblem = problem;
+        UI.instance.SetProblemText(problems[curProblem]);
+        remainingTime = timePerProblem;
+        
+    }
+
+
+    // player enters the correct tube
+    void CorrectAnswer()
+    {
+        if (problems.Length - 1 == curProblem)
+            Win();
+        else
+            SetProblem(curProblem + 1);
+    }
+    //player enters the incorrect tube
+    void IncorrectAnswer()
+    {
+        player.Stun();
+    }
+    public void OnPlayerEnterTube(int tube)
+    {
+        if (tube == problems[curProblem].correctTube)
+        {
+            CorrectAnswer();
+
+            player.transform.position = new Vector2(-0.05f, -3.34f);
+        }
+        else
+            IncorrectAnswer();
+    }
+    void Update()
+    {
+        remainingTime -= Time.deltaTime;
+        if (remainingTime <= 0.0f)
+        {
+            Lose();
+        }
+    }
+
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public Text problemText;                
+    public Text[] answersTexts;             
+    public Image remainingTimeDial;         
+    private float remainingTimeDialRate;    
+    public Text endText;
+
+    public static UI instance;
+    void Awake()
+    {
+        instance = this;//Creating instance for accessing through other script
+    }
+    void Start()
+    {
+        {
+            remainingTimeDialRate = 1.0f / GameManager.instance.timePerProblem;//exp 8 seconds will divide dial 8 times so with Time.deltaTime that will just work as a clock
+        }
+    }
+    void Update()
+    {
+        remainingTimeDial.fillAmount = remainingTimeDialRate * GameManager.instance.remainingTime;
+    }
+
+    public void SetProblemText(Problem problem)
+    {
+        string operatorText = "";
+        switch (problem.operation)
+        {
+            case MathsOperation.Addition: operatorText = " + "; break;
+            case MathsOperation.Subtraction: operatorText = " - "; break;
+            case MathsOperation.Multiplication: operatorText = " x "; break;
+            case MathsOperation.Division: operatorText = " ÷ "; break;
+        }
+        problemText.text = problem.firstNumber + operatorText + problem.secondNumber;
+        for (int index = 0; index < answersTexts.Length; ++index)
+        {
+            answersTexts[index].text = problem.answers[index].ToString();
+        }
+    }
+    public void SetEndText(bool win)
+    {
+        endText.gameObject.SetActive(true);
+        if (win)
+        {
+            endText.text = "You Win!";
+            endText.color = Color.green;
+        }
+        else
+        {
+            endText.text = "Game Over!";
+            endText.color = Color.red;
+        }
+    }
+}
